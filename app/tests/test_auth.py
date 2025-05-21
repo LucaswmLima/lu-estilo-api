8510b10870de8f748e2f9062c0bac41f35f2d24d
Lucas William <lucaswilliamml@gmail.com>
2025-05-21 18:01:45 -0300

    update test auth and config to use .env PORT for tests

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -1,9 +1,10 @@
 import pytest
 from httpx import AsyncClient
+from app.core.config import BASE_URL
 
 @pytest.mark.asyncio
 async def test_register_user():
-    async with AsyncClient(base_url="http://localhost:8000") as ac:
+    async with AsyncClient(base_url=BASE_URL) as ac:
         response = await ac.post("/auth/register", json={"email": "test@example.com", "password": "senha123"})
     assert response.status_code == 200
     data = response.json()
@@ -12,7 +13,7 @@ async def test_register_user():
 
 @pytest.mark.asyncio
 async def test_register_duplicate_user():
-    async with AsyncClient(base_url="http://localhost:8000") as ac:
+    async with AsyncClient(base_url=BASE_URL) as ac:
         await ac.post("/auth/register", json={"email": "testdup@example.com", "password": "senha123"})
         response = await ac.post("/auth/register", json={"email": "testdup@example.com", "password": "senha123"})
     assert response.status_code == 400
