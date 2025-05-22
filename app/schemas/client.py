5105664d2e15140876bf7d98d64cd7a10ff8af94
Lucas William <lucaswilliamml@gmail.com>
2025-05-22 20:49:35 -0300

    update client schema

---
+++ app/schemas/client.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr, constr
+from typing import Optional
+
+# Define o tipo CPF com restrições de tamanho (11 dígitos)
+CPFStr = constr(min_length=11, max_length=11)
+
+class ClientBase(BaseModel):
+    name: str
+    email: EmailStr
+    cpf: CPFStr # type: ignore
+
+class ClientCreate(ClientBase):
+    pass
+
+class ClientUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    cpf: Optional[CPFStr] = None # type: ignore
+
+class ClientOut(ClientBase):
+    id: int
+
+    class Config:
+        orm_mode = True
