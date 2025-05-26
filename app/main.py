71714b999ad28c9188eb5951a751facbea92ceb4
Lucas William <lucaswilliamml@gmail.com>
2025-05-26 15:04:39 -0300

    Fix sentry ENV import problem

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+import os
+from fastapi import APIRouter, FastAPI
+import sentry_sdk
 from app.db.database import engine, Base
 from app.routes import auth_route, order_route, client_route, product_route
-from app.utils.sentry import init_sentry
 
-init_sentry()
+from dotenv import load_dotenv
+load_dotenv()
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    send_default_pii=True,
+    traces_sample_rate=1.0
+)
+
 
 Base.metadata.create_all(bind=engine)
 
@@ -13,3 +22,13 @@ app.include_router(auth_route.router)
 app.include_router(client_route.router)
 app.include_router(product_route.router)
 app.include_router(order_route.router)
+
+
+
+router_sentry = APIRouter(prefix="/sentry-error", tags=["sentry"])
+
+@router_sentry.get("/debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
+
+app.include_router(router_sentry)
